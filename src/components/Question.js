d8929d47305feb86b9331208fe3ea35e2ea1a411
husseinahmed <hussenahmedkhalif122@gmail.com>
2024-07-15 17:54:07 +0300

    useEffect lab complete

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    if (timeRemaining > 0) {
+      const timer = setTimeout(() => {
+        setTimeRemaining(timeRemaining - 1);
+      }, 1000);
+
+      // Cleanup timeout on unmount
+      return () => clearTimeout(timer);
+    } else {
+      // Reset timer and call onAnswered with false
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
@@ -30,3 +43,5 @@ function Question({ question, onAnswered }) {
 }
 
 export default Question;
+
+
